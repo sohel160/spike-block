f5d1e9b1bda34af6415d8b1969d4640cdb9d32c1
sohel160 <sohelcmc53rd@gmail.com>
2026-04-07 11:01:47 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,60 +2,82 @@ export default {
   async fetch(request) {
 
     const url = new URL(request.url)
-    const token = url.searchParams.get("token")
 
     // token protection
-    if (token !== "abc123") {
+    if (url.searchParams.get("token") !== "abc123") {
       return new Response("Forbidden", { status: 403 })
     }
 
-    // detect client
+    // allow only clash clients
     const ua = request.headers.get("User-Agent") || ""
-    const accept = request.headers.get("Accept") || ""
 
-    // allow only Clash / FiClash clients
-    const allowedClients = [
+    const allowedUA = [
       "Clash",
       "clash",
       "ClashMeta",
       "ClashforWindows",
       "ClashX",
       "Stash",
-      "Shadowrocket",
       "FiClash"
     ]
 
     let allowed = false
 
-    for (const client of allowedClients) {
-      if (ua.includes(client)) {
+    for (const a of allowedUA) {
+      if (ua.includes(a)) {
         allowed = true
         break
       }
     }
 
-    // block browsers
-    if (!allowed && accept.includes("text/html")) {
+    if (!allowed) {
       return new Response("404 Not Found", { status: 404 })
     }
 
-    // hidden proxy config
     const config = `
-proxies:
-  - name: "proxy1"
-    type: http
-    server: 44.48.108.333
-    port: 5452
+port: 7890
+socks-port: 7891
+allow-lan: false
+mode: rule
 
-  - name: "proxy2"
+proxy-providers:
+  main:
     type: http
-    server: 44.48.108.333
-    port: 5452
+    url: https://clash-worker.darkblazespuky.workers.dev/?token=abc123
+    interval: 3600
+    path: ./providers.yaml
+    health-check:
+      enable: true
+      interval: 600
+      url: http://www.gstatic.com/generate_204
+
+proxy-groups:
+
+- name: AUTO
+  type: select
+  use:
+    - main
+
+rules:
+- MATCH,AUTO
+
+# actual proxies
+proxies:
+
+- name: proxy1
+  type: http
+  server: 144.48.108.121
+  port: 5452
+
+- name: proxy2
+  type: http
+  server: 144.48.108.122
+  port: 5452
 `
 
     return new Response(config, {
       headers: {
-        "Content-Type": "text/plain; charset=utf-8"
+        "Content-Type": "text/plain"
       }
     })
 
