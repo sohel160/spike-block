284e28ecbf7acd10a89938124a2fb32d31dfab09
sohel160 <sohelcmc53rd@gmail.com>
2026-04-07 11:17:16 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,63 +1,20 @@
-export default {
-  async fetch(request) {
+port: 7890
+socks-port: 7891
+mode: rule
 
-    const url = new URL(request.url)
-
-    // token check
-    if (url.searchParams.get("token") !== "abc123") {
-      return new Response("Forbidden", { status: 403 })
-    }
-
-    // allow only Clash clients
-    const ua = request.headers.get("User-Agent") || ""
-
-    const allowedUA = [
-      "Clash",
-      "clash",
-      "ClashMeta",
-      "ClashforWindows",
-      "ClashX",
-      "Stash"
-    ]
-
-    let allowed = false
-
-    for (const a of allowedUA) {
-      if (ua.includes(a)) {
-        allowed = true
-        break
-      }
-    }
-
-    if (!allowed) {
-      return new Response("404 Not Found", { status: 404 })
-    }
-
-    const config = `
 proxy-providers:
-  myprovider:
+  hidden:
     type: http
-    url: "https://proxies-worker.darkblazespuky.workers.dev/proxies?token=abc123"
+    url: https://proxies-worker.darkblazespuky.workers.dev/?token=abc123
     interval: 3600
-    path: ./proxies.yaml
-    health-check:
-      enable: true
-      url: http://www.gstatic.com/generate_204
-      interval: 10
+    path: ./hidden.yaml
 
 proxy-groups:
-- name: "FREE"
-  type: load-balance
-  strategy: round-robin
+
+- name: AUTO
+  type: select
   use:
-  - myprovider
+    - hidden
 
 rules:
-- MATCH,FREE
-`
-
-    return new Response(config, {
-      headers: { "Content-Type": "text/plain" }
-    })
-  }
-}
+- MATCH,AUTO
