6ca117151766e167c4613b0208522a89b9f4afbd
sohel160 <sohelcmc53rd@gmail.com>
2026-04-10 09:56:49 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -38,7 +38,7 @@ export default {
 proxy-providers:
   myprovider:
     type: http
-    url: "https://clash-worker.darkblazespuky.workers.dev/?token=abc123"
+    url: 
     interval: 3600
     path: ./proxies.yaml
     health-check:
