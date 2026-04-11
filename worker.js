ffd86c67950fb93f54ccb003ed5bfeeb7f9e0198
sohel160 <sohelcmc53rd@gmail.com>
2026-04-11 20:16:30 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,61 +3,53 @@ export default {
 
     const url = new URL(request.url)
 
+    // token protection
     if (url.searchParams.get("token") !== "abc123") {
       return new Response("Forbidden", { status: 403 })
     }
 
+    // allow only FiClash
     const ua = request.headers.get("User-Agent") || ""
 
-    const allowedUA = [
-      "Clash",
-      "clash",
-      "ClashMeta",
-      "ClashforWindows",
-      "ClashX",
-      "Stash",
-      "FiClash"
-    ]
-
-    let allowed = false
-
-    for (const a of allowedUA) {
-      if (ua.includes(a)) {
-        allowed = true
-        break
-      }
+    if (!ua.includes("FiClash")) {
+      return new Response("Access Denied", { status: 403 })
     }
 
-    if (!allowed) {
-      return new Response("404 Not Found", { status: 404 })
-    }
+    const config = `
+port: 7890
+socks-port: 7891
+allow-lan: true
+mode: rule
+log-level: info
 
-    const proxies = `
-    proxies:
-  - name: "speed"
+proxy-providers:
+  main:
     type: http
-    server: 103.109.96.20
-    port: 9862
-  - name: "nice"
-    type: http
-    server: 103.69.150.138
-    port: 9859
+    url: "https://spike-block.darkblazespuky.workers.dev/proxies?token=abc123"
+    interval: 3600
+    path: ./main.yaml
+
 proxy-groups:
-  - name: "FREE"
-    type: load-balance
-    strategy: round-robin
-    proxies:
-      - "speed"
-      - "nice"
-    url: "http://www.gstatic.com/generate_204" # Required for load-balance/health checks
-    interval: 10
+
+- name: SPEED
+  type: select
+  proxies:
+    - speed
+    - nice
+
+- name: FAST
+  type: load-balance
+  strategy: round-robin
+  proxies:
+    - speed
+    - nice
+  interval: 60
 
 rules:
-  - MATCH,FREE
-
+- MATCH,SPEED
 `
 
-    return new Response(proxies, {
+    return new Response(config, {
       headers: {
         "Content-Type": "text/plain"
       }
